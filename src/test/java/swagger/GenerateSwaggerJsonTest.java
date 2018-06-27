252d912f3bded170b2ee35aceba3124fa71bde42
acro-sakamoto <sakamoto@acroquest.co.jp>
2018-06-27 18:48:49 +0900

    途中

--- src/test/java/swagger/GenerateSwaggerJsonTest.java
+++ src/test/java/swagger/GenerateSwaggerJsonTest.java
@@ -3,6 +3,9 @@ package swagger;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,12 +17,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import io.github.robwin.markup.builder.MarkupLanguage;
-import springfox.documentation.staticdocs.Swagger2MarkupResultHandler;
-
 @SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
-public class Swagger2MarkupTest {
+public class GenerateSwaggerJsonTest {
 
     @Autowired
     private WebApplicationContext context;
@@ -33,15 +33,9 @@ public class Swagger2MarkupTest {
 
     @Test
     public void convertSwaggerToAsciiDoc() throws Exception {
-        mockMvc.perform(get("/v2/api-docs").accept(MediaType.APPLICATION_JSON)).andDo(
-                Swagger2MarkupResultHandler.outputDirectory("src/docs/asciidoc/generated").build()).andExpect(
-                        status().isOk());
+        mockMvc.perform(get("/v2/api-docs").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andDo(
+                resultHandler -> Files.write(Paths.get("target/swagger.json"),
+                        resultHandler.getResponse().getContentAsByteArray()));
     }
 
-    @Test
-    public void convertSwaggerToMarkdown() throws Exception {
-        mockMvc.perform(get("/v2/api-docs").accept(MediaType.APPLICATION_JSON)).andDo(
-                Swagger2MarkupResultHandler.outputDirectory("src/docs/markdown/generated").withMarkupLanguage(
-                        MarkupLanguage.MARKDOWN).build()).andExpect(status().isOk());
-    }
 }
